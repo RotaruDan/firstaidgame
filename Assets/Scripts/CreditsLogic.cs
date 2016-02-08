a6ab8e3822a9ebe37439d7516f45a2b0d00264bb
RotaruDan <drotaru@ucm.es>
2016-02-08 15:42:42 +0100

    Adds Credits scene

---
+++ Assets/Scripts/CreditsLogic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditsLogic : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GoToMainMenu()
+    {
+        Image image = GetComponent<Image>();
+
+        if (image != null)
+        {
+            Sprite sprite = image.overrideSprite;
+            if (sprite != null && sprite.name == "Creditos_02")
+            {
+                SceneManager.LoadScene("Final"); // TODO change to Main Menu
+            }
+        }
+    }
+}
