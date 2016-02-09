e9fa8aa0074e4e98d09e7c94824c4386341ded8c
RotaruDan <drotaru@ucm.es>
2016-02-09 14:24:41 +0100

    Adds PlayVideo script

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+
+public class PlayVideo : MonoBehaviour
+{
+
+    public MovieTexture movie;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        movie.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
