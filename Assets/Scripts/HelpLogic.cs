bdf04bc13d7f6abda025ae72ee2df426c5cf4113
RotaruDan <drotaru@ucm.es>
2016-02-09 11:52:38 +0100

    Adds Ayudascene

---
+++ Assets/Scripts/HelpLogic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HelpLogic : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GoToMainMenu()
+    {
+        Image image = GetComponent<Image>();
+        
+        if (image != null)
+        {
+            Sprite sprite = image.overrideSprite;
+            if (sprite != null && sprite.name == "Ayuda2")
+            {
+                SceneManager.LoadScene("Inicio"); // TODO change to Main Menu
+            }
+        }
+    }
+}
