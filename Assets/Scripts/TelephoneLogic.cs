e12edc66713731b6d0d519bee1764d2a9b624331
RotaruDan <drotaru@ucm.es>
2016-02-10 18:38:23 +0100

    Adds TelephoneLogic Script

---
+++ Assets/Scripts/TelephoneLogic.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TelephoneLogic : MonoBehaviour
+{
+
+    public Sprite telefonoEspera;
+    public Sprite hablandoTelefono;
+
+    private Sprite sourceImage;
+
+    // Use this for initialization
+    void Start()
+    {
+        sourceImage = GetComponent<Image>().overrideSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool hablandoTlf = Flags.ValorDe("HablandoTelefono");
+        if (hablandoTlf)
+        {
+            if (sourceImage != hablandoTelefono)
+            {
+                sourceImage = hablandoTelefono;
+            }
+        }
+        else if (sourceImage != telefonoEspera)
+        {
+            sourceImage = telefonoEspera;
+        }
+    }
+
+    public void OnClick()
+    {
+
+    }
+}
